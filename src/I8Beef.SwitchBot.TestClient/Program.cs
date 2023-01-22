be153c46d1923dab3a9c4db654115705d011d239
Michael Hallock <mike@hallock.net>
2023-01-21 19:17:25 -0500

    Cleanup

--- src/I8Beef.SwitchBot.TestClient/Program.cs
+++ src/I8Beef.SwitchBot.TestClient/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace I8Beef.SwitchBot.TestClient
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace I8Beef.SwitchBot.TestClient
 {
     public class Program
     {
